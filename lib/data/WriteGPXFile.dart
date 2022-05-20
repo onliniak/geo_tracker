a0d5e7f62086cbf2b3bbf61021d7023cd9eee163
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2022-05-20 18:41:09 +0200

    fix: Write permissions.

--- lib/data/WriteGPXFile.dart
+++ lib/data/WriteGPXFile.dart
@@ -2,8 +2,14 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 Future<File?> writeGPX(String counter, bool write) async {
+  var status = await Permission.storage.status;
+  if (!status.isGranted) {
+    await Permission.storage.request();
+  }
+
   final directory = await getExternalStorageDirectory();
   final path = directory?.path;
   final file = File("$path/my.gpx");
