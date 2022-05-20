aa9d866b1486f83c2819ce78ab6ad64196a78571
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2022-05-20 16:53:03 +0200

    fix: Add path_provider dependency. Change the location of the saved file.

--- lib/data/WriteGPXFile.dart
+++ lib/data/WriteGPXFile.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
 import 'dart:io';
 
-Future<File?> writeCounter(String counter, bool write) async {
-  final file = File('/data/data/io.github.onliniak.geo_tracker/files/my.gpx');
+import 'package:path_provider/path_provider.dart';
+
+Future<File?> writeGPX(String counter, bool write) async {
+  final directory = await getExternalStorageDirectory();
+  final path = directory?.path;
+  final file = File("$path/my.gpx");
 
   if (write == true) {
     return file.writeAsString(counter);
