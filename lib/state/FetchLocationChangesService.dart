bad674d8e903fb8bcf57db52b1f16f0fa8397221
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2022-05-21 12:53:51 +0200

    fix: DateTime immutable.

--- lib/state/FetchLocationChangesService.dart
+++ lib/state/FetchLocationChangesService.dart
@@ -5,10 +5,10 @@ import 'package:geolocator/geolocator.dart';
 import '../data/WriteGPXFile.dart';
 
 void saveLocationAsGPX(LocationSettings locationSettings) {
-  DateTime time = DateTime.now();
   StreamSubscription<Position> positionStream =
       Geolocator.getPositionStream(locationSettings: locationSettings)
           .listen((Position? position) {
+        DateTime time = DateTime.now();
     writeGPX('''
     
       <trkpt lat="${position?.latitude.toString()}" lon="${position?.longitude.toString()}"><time>${time.year}-${time.month}-${time.day}T${time.hour}:${time.minute}:${time.second}Z</time></trkpt>''',
